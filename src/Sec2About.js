f063c832ea145e239e2fb71103dfb41e493b5755
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-01-18 16:30:12 +0400

    Update Sec2About.js
    read more

--- src/Sec2About.js
+++ src/Sec2About.js
@@ -1,7 +1,28 @@
 import React from "react";
 import bg from "./images/bg.png";
 import RabeeCV from './CV/Rabee Aljaramani Web developer.pdf';
-function Sec2About() {
+class Sec2About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  myFunction(e){
+    e.preventDefault();
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }  
+  render(){
   return (
     <section className="section about" id="about">
       <div className="title">
@@ -20,14 +41,16 @@ function Sec2About() {
             Coming from an Informatics Engineering background, I have extensive experience in IT fields.
             I come from Syria where I studied to be a Computer Engineer because I believe this technology is the future of humanity and it will make our life as we dreamed about it long time ago where everything around us is controlled and automated.
             I have always had a passion for learning new things and technologies.
-            After the war started in Syria, I left for Lebanon and worked as IT support.
+            <span id="dots">...</span><span id="more">
+        After the war started in Syria, I left for Lebanon and worked as IT support.
             In the positions I held I tried always to add automation to my work to make it faster and more accurate, in addition, searching for the problems with end user’s workflow and try to develop a programs or tools to help them.
             I always found myself drawn to software development which is very close to our daily duties and problems and after the huge progress of the internet, I took the decision to become a web developer and worked tirelessly after hours and weekend to teach myself web development by doing online courses and building my own projects.
             This journey has given me all the tools to always improve myself and the endurance to face and succeed in any challenge presented to me.
             I believe if I am given the chance to prove myself, that I will proof to be a valuable asset to your company that will go far and beyond to reach the goals of the organization.
-
-          </p>
-
+        </span>
+        <button onClick={this.myFunction} id="myBtn" className='btn-readmore'>Read more</button><br/>
+        </p>
+        
           <a href={RabeeCV} className="btn" download="Rabee Aljaramani Web developer.pdf">
             Download CV
           </a>
@@ -35,5 +58,5 @@ function Sec2About() {
       </div>
     </section>
   );
-}
+}}
 export default Sec2About;
