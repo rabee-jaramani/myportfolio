9b294c6f1120f6cd644d3e06042ca1aa15f30633
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-01-18 19:35:15 +0400

    Update Sec2About.js

--- src/Sec2About.js
+++ src/Sec2About.js
@@ -45,7 +45,7 @@ class Sec2About extends React.Component {
     
             Coming from an Informatics Engineering background and have 9+ years experience in the IT field, ranging from IT support to software development.
     Passionate about web development and seeking to grow in the role of a web developer by using my React, Redux, HTML and CSS skills to deliver coding excellence.
-            I come from Syria where I studied to be a Computer Engineer because I believe this technology is the future of humanity and it will make our life as we dreamed about it long time ago where everything around us is controlled and automated.
+            I came from Syria where I studied to be a Computer Engineer because I believe this technology is the future of humanity and it will make our life as we dreamed about it long time ago where everything around us is controlled and automated.
             I have always had a passion for learning new things and technologies.<span id="dots">...</span><span id="more"><br/>After the war started in Syria, I left for Lebanon and worked as IT support.
             In the positions I held I tried always to add automation to my work to make it faster and more accurate, in addition, searching for the problems with end user’s workflow and try to develop a programs or tools to help them.
             I always found myself drawn to software development which is very close to our daily duties and problems and after the huge progress of the internet, I took the decision to become a web developer and worked tirelessly after hours and weekend to teach myself web development by doing online courses and building my own projects.
