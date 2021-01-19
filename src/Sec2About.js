c2cb40b618af332e0e1bb77884be2213d8bacb37
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-01-19 18:54:09 +0400

    Update Sec2About.js

--- src/Sec2About.js
+++ src/Sec2About.js
@@ -38,20 +38,13 @@ class Sec2About extends React.Component {
             I'm <span id="type2"></span>
           </h1>
           <p className="aboutme-p">
-            Coming from an Informatics Engineering background and have 9+ years experience in the IT field, ranging from IT support to software development.
-    Passionate about web development and seeking to grow in the role of a web developer by using my React, Redux, HTML and CSS skills to deliver coding excellence.
-            I came from Syria where I studied to be a Computer Engineer because I believe this technology is the future of humanity and it will make 
-    our life as we dreamed about it long time ago where everything around us is controlled and automated.
-            I have always had a passion for learning new things and technologies.<span id="dots">...</span><span id="more"><br/>After the war started in my home,
-    I left for Lebanon and worked as IT support.
-            In the positions I held I tried always to add automation to my work to make it faster and more accurate, in addition,
-    searching for the problems with end user’s workflow and try to develop a programs or tools to help them.
-            I always found myself drawn to software development which is very close to our daily duties and problems and after the huge progress of the internet,
-    I took the decision to become a web developer and worked tirelessly after hours and weekend to teach myself web development by doing online courses and building
-    my own projects.
-            This journey has given me all the tools to always improve myself and the endurance to face and succeed in any challenge presented to me.
-            I believe if I am given the chance to prove myself, that I will proof to be a valuable asset to your company that will go far and beyond
-    to reach the goals of the organization.
+           I come from an Informatics Engineering background and have 9+ years experience in the IT field, ranging from IT support to software development.
+    Passionate about web development and seeking to grow in the role of a web developer by using my React, Redux, HTML and CSS skills
+    to deliver coding excellence..<span id="dots">...</span><span id="more"><br/>
+    I came from Syria where I studied to be a Computer Engineer because I have always believed that  technology is the future of humanity and it will shape our lives in ways that we have only been able to dream about. . 
+After the war started in my home, I left for Lebanon and worked as IT support. I have a great passion for learning new skills and technologies and therefore I have automated workflows by developing new programs and tools in every position I have held. 
+  I made the decision to become a web developer and worked tirelessly after hours and weekends to teach myself web development by doing online courses and building my own projects. This journey has given me all the tools to always improve myself and the endurance to face and succeed in any challenge presented to me. I believe if I am given the chance to prove myself, that I will prove to be a valuable asset to your company that will go far and beyond to reach the goals of the organization.
+
         </span>
         <button onClick={this.myFunction} id="myBtn" className='btn-readmore'>Read More</button><br/>
         </p>
